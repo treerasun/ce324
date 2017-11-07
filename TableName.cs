9a3a05229a0934f952d2c55a0bb1cf60ec1b69c2
treerasun <treerasun1234@gmail.com>
2017-11-07 11:35:33 +0700

    Create TableName.cs

---
+++ TableName.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FAPerformanceWeb
+{
+    public class TableName
+    {
+        public static string MC = "MC";
+        public static string MC2 = "MC2";
+        public static string MC1 = "MC3";
+        public static string MC_ALARM1 = "MC_ALARM1";
+    }
+}
